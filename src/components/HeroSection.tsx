64055ebb3242b04c813cd14a58ee64e8ee905fa6
diemorester <rahadianfikar@gmail.com>
2025-11-13 20:16:41 +0700

    hero section created

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,12 @@
 export default function HeroSection() {
     return (
-        <section id="home" className="w-full h-screen bg-black text-center text-dummy-white place-content-center">
-            Hero
+        <section id="home" className="w-full h-screen relative bg-black text-center text-dummy-white place-content-center pt-20">
+            <img
+                src="/hero.webp"
+                alt="Hero Image"
+                loading="lazy"
+                className="w-full h-screen object-cover"
+            />
         </section>
     )
 }
